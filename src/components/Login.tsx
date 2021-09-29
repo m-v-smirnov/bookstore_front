ee7f824f1786ba60ae316aa721c3c70194f07410
m-v-smirnov <m.v.smirnoff86@yandex.ru>
2021-09-29 23:02:33 +0300

    Added login/register page and added styled-component for it

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import {UserLoginOptions,userLogin} from '../api/userApi';
+import { UserLoginOptions, UserCreateOptions, userLogin, userCreate } from '../api/userApi';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    
+.container {
+  background-color: lightgreen;
+  padding: 20px 20px;
+  margin: 20px auto;
+  width: 380px;
+  text-align: center;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+
+  &__form {
+  display: flex;
+  flex-direction: column;
+  }
+  &__input {
+    margin-bottom: 20px;
+    padding: 5px;
+    border-radius: 5px;
+    width: auto;
+  }
+  &__label {
+    align-self: flex-start;
+  }
+  &__button--blue {
+    padding: 5px;
+    font-size: 24px;
+    font-weight: 600;
+    background-color: blueviolet;
+    border-radius: 18px;
+    border-width: 2px;
+    border: 1px solid white;
+    color: white;
+  }
+  &__button--white {
+    padding: 5px;
+    font-size: 24px;
+    font-weight: 600;
+    background-color: white;
+    border-radius: 18px;
+    border-width: 2px;
+    border: 1px solid black;
+    color: black;
+  }
+}
+
+`;
 
 type Props = {
 };
@@ -7,14 +57,17 @@ type Props = {
 export const Login: React.FC<Props> = (props) => {
   const [registerPage, setRegisterPage] = useState(false);
   const [sendData, setSendData] = useState(false);
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [dob, setDob] = useState('');
 
   useEffect(() => {
+    if (registerPage) return;
     if (!sendData) return;
     const options: UserLoginOptions = {
-      email: loginEmail,
-      password: loginPassword,
+      email,
+      password,
     };
     const res: any = userLogin(options);
     console.log(res);
@@ -22,54 +75,187 @@ export const Login: React.FC<Props> = (props) => {
   },
     [sendData]
   );
+  useEffect(() => {
+    if (!registerPage) return;
+    if (!sendData) return;
+    const options: UserCreateOptions = {
+      name,
+      email,
+      dob,
+      password,
+    };
+    const res: any = userCreate(options);
+    console.log(res);
+    setSendData(false);
+  },
+    [sendData]
+  );
 
   return (
-    <div>
-      <h2>Enter your login:</h2>
+    <StyledDiv>
+      {!registerPage
+        ? <> {/* Login form*/}
+          <div className='container'>
+            <h3>Sign into your Bookstore account</h3>
+            <form className='container__form'>
+              <label
+                className='container__label'
+                htmlFor="GET-email"
+              >
+                Email:
+              </label>
+              <input
+                className='container__input'
+                id="GET-email"
+                type="text"
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setEmail(e.target.value);
+                }}
+              >
+              </input>
+              <label
+                className='container__label'
+                htmlFor="GET-pass"
+              >
+                Password:
+              </label>
+              <input
+                className='container__input'
+                id="GET-pass"
+                type="text"
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              >
+              </input>
+              <button
+                className='container__button--blue'
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSendData(true);
+                }}
+              >
+                Sign in
+              </button>
 
+            </form>
+            <h3>Don't have an account?</h3>
+            <button
+              className='container__button--white'
+              onClick={(e) => {
+                e.preventDefault();
+                setRegisterPage(true);
 
-      <form>
-        <label htmlFor="GET-email">
-          Email:
-        </label>
-        <input
-          id="GET-email"
-          type="text"
-          name="email"
-          value={loginEmail}
-          onChange={(e) => {
-            setLoginEmail(e.target.value);
-          }
-          }
-        >
-        </input>
-        <label htmlFor="GET-pass">
-          Password:
-        </label>
-        <input
-          id="GET-pass"
-          type="text"
-          name="password"
-          value={loginPassword}
-          onChange={(e) => {
-            setLoginPassword(e.target.value);
-          }
-          }
-        >
-        </input>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setSendData(true);
+              }}
+            >
+              Create an account
+            </button>
+          </div>
+        </>
+        : <> {/* Register form*/}
+          <div className='container'>
+            <h3>Create your Bookstore account</h3>
+            <form className='container__form'>
+              <label
+                className='container__label'
+                htmlFor="GET-name"
+              >
+                Name:
+              </label>
+              <input
+                className='container__input'
+                id="GET-name"
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              >
+              </input>
+              <label
+                className='container__label'
+                htmlFor="GET-email"
+              >
+                Email:
+              </label>
+              <input
+                className='container__input'
+                id="GET-email"
+                type="text"
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              >
+              </input>
+              <label
+                className='container__label'
+                htmlFor="GET-dob"
+              >
+                Date of birth:
+              </label>
+              <input
+                className='container__input'
+                id="GET-dob"
+                type="date"
+                name="dob"
+                value={dob}
+                onChange={(e) => {
+                  setDob(e.target.value);
+                }}
+              >
+              </input>
+              <label
+                className='container__label'
+                htmlFor="GET-pass"
+              >
+                Password:
+              </label>
+              <input
+                className='container__input'
+                id="GET-pass"
+                type="text"
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              >
+              </input>
+              <button
+                className='container__button--blue'
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSendData(true);
 
-          }}
-        >
-          Login
-        </button>
-      </form>
+                }}
+              >
+                Create an account
+              </button>
 
+            </form>
+            <h3>Already have an account?</h3>
+            <button
+              className='container__button--white'
+              onClick={(e) => {
+                e.preventDefault();
+                setRegisterPage(false);
 
-    </div>
+              }}
+            >
+              Sign in
+            </button>
+          </div>
+        </>
+      }
+    </StyledDiv>
 
   );
 }
